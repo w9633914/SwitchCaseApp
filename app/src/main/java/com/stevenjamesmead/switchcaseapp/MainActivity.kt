480d8aace6b92d640ef2294233119eec0710ca5f
w9633914 <w9633914@live.tees.ac.uk>
2023-10-17 17:32:09 +0100

    Changed the text

--- app/src/main/java/com/stevenjamesmead/switchcaseapp/MainActivity.kt
+++ app/src/main/java/com/stevenjamesmead/switchcaseapp/MainActivity.kt
@@ -83,7 +83,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             onClick = { switchCaseMessage = switchCase(userMessage) },
             modifier = Modifier.fillMaxWidth()) {
 
-            Text(text = "Update")
+            Text(text = "Change the case")
         }
 
         SwitchCaseText(
